ff8444c0143c69aa71e71f7e2b7b54fdfed9deb2
Victor Hugo <victorhugofp11@hotmail.com>
2021-03-16 13:18:28 -0700

    Creacion de archivos y base del codigo

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+using namespace std;
+
+int op;
+double raire, masa;
+
+void Menu(){
+
+    cout<<"Resolución de problema de un paracaidista"<<endl;
+    cout<<"Digite la resistencia al aire: "<<endl;  cin>>raire;
+    cout<<"Digite la masa: "<<endl; cin>>masa;
+
+}
+
+
+
+int main () {
+
+    Menu();
+
+
+
+
+
+
+
+
+    return 0;
+}
