1af5bb39568f5b82de6a3e5c2e4ffd7cea332e2c
Victor Hugo <victorhugofp11@hotmail.com>
2021-03-16 14:21:35 -0700

    Lectura del txt hecho y transformado a double

--- main.cpp
+++ main.cpp
@@ -1,18 +1,61 @@
 #include <iostream>
 #include<fstream>
 #include<stdlib.h>
-#include<string.h>
+#include<string>
 #include<math.h>
 using namespace std;
 
 int op;
-double raire, masa;
+double raire, masa, graveda;
+string gravity;
+fstream gravedad ("gravedad.txt");
+
+
 
 void Menu(){
 
-    cout<<"Resolución de problema de un paracaidista"<<endl;
-    cout<<"Digite la resistencia al aire: "<<endl;  cin>>raire;
+    cout<<"Resolucion de problema de un paracaidista"<<endl;
+    cout<<"¿Desea cambiar la gravedad?: \n 1.-Si \n 2.-No \n : ";
+    cin>>op;
+
+    switch (op) {
+    case 1:
+    // Cambiar la gravedad
+
+
+
+
+
+
+
+        break;
+
+    case 2:
+    //Continuar con la establecida
+        if(!gravedad.is_open()){
+        gravedad.open("gravedad.txt", ios::in);
+        }
+
+        while(getline(gravedad, gravity)){
+        
+        }
+        gravedad.close();
+
+        graveda = stod(gravity);
+        cout<<"\n"<<"La gravedad actual es: "<<graveda<<endl;
+
+    break;
+
+
+
+    default:
+    cout<<"¡¡¡Digite una opcion valida!!!";
+    Menu();
+        break;
+    }
+    cout<<"\nDigite la resistencia al aire: "<<endl;  cin>>raire;
     cout<<"Digite la masa: "<<endl; cin>>masa;
+
 
 }
 
@@ -20,14 +63,6 @@ void Menu(){
 
 int main () {
 
-    Menu();
-
-
-
-
-
-
-
-
+        Menu();
     return 0;
 }
