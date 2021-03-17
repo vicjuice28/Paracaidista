f992377b1c152321a24dd2d919f72221f0b3c8ed
Victor Hugo <victorhugofp11@hotmail.com>
2021-03-16 18:56:47 -0700

    Programa terminado funcionando correctamente

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include<fstream>
-#include<stdlib.h>
 #include<string>
 #include<math.h>
+#include "utils.hpp"
 using namespace std;
 
-int op;
+int op, init, fin, paso, i;
 double raire, masa, graveda;
-string gravity;
-fstream gravedad ("gravedad.txt");
+double v = 0;
+string gravity, newgrav;
+ifstream gravedad ("gravedad.txt");
+ofstream gravedad2("gravedad.txt");
+
 
 
 
@@ -17,11 +20,22 @@ void Menu(){
     cout<<"Resolucion de problema de un paracaidista"<<endl;
     cout<<"¿Desea cambiar la gravedad?: \n 1.-Si \n 2.-No \n : ";
     cin>>op;
+    
 
     switch (op) {
     case 1:
     // Cambiar la gravedad
+        cout<<"Escriba la nueva gravedad: "; cin>>newgrav;
+       if(!gravedad2.is_open()){
+           gravedad2.open("gravedad.txt", ios::out);
+       }
+       
+        
+        gravedad2 << newgrav;
+        gravedad2.close();
+        graveda = stod(newgrav);
 
+        cout<<"La gravedad es: "<<graveda<<endl;
 
 
 
@@ -33,11 +47,12 @@ void Menu(){
     case 2:
     //Continuar con la establecida
         if(!gravedad.is_open()){
-        gravedad.open("gravedad.txt", ios::in);
+             gravedad.open("gravedad.txt", ios::in);
         }
+       
 
         while(getline(gravedad, gravity)){
-        
+
         }
         gravedad.close();
 
@@ -55,6 +70,32 @@ void Menu(){
     }
     cout<<"\nDigite la resistencia al aire: "<<endl;  cin>>raire;
     cout<<"Digite la masa: "<<endl; cin>>masa;
+    cout<<"Digite en que segundo iniciara: "<<endl; cin>>init;
+    cout<<"Digite en que segundo acabara: "<<endl; cin>>fin;
+    cout<<"Digite cuantos pasos en tiempo hara: "<<endl; cin>>paso;
+
+
+
+
+
+}
+
+
+
+
+void Tabla(){
+    system(CLEAN);
+    cout<<"t,s"<<"\t"<<"v m/s"<<endl;
+    cout<<"--------------"<<endl;
+   for ( i = init; i <= fin; i+=paso){
+        v = v+ (graveda - ((raire / masa) * v)) * paso;
+        cout<<i<<"\t"<<v<<endl;
+
+
+
+    }
+
+   
 
 
 }
@@ -62,7 +103,18 @@ void Menu(){
 
 
 int main () {
+    //Abrir menu me borra contenido en archivo, así que se construye antes
+        if(!gravedad2.is_open()){
+           gravedad2.open("gravedad.txt", ios::out);
+            }
+       
+        
+        gravedad2 << 9.8;
+        gravedad2.close();
+
+
 
         Menu();
+        Tabla();
     return 0;
 }
---
+++ utils.hpp
@@ -0,0 +1,13 @@
+#ifndef UTILS
+#define UTILS
+
+#ifdef __linux__
+#define CLEAN "clear"
+#endif // __linux__
+ 
+#ifdef __MINGW32__
+#define CLEAN "cls"
+#endif // __MINGW32__
+
+
+#endif
